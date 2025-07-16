b14eb50b63e8b406a6859949fee686ca64c8042e
Austin <ankhoa1212@gmail.com>
2025-07-16 01:28:06 -0700

    add map functionality

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
-import "strings"
-import "bufio"
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type Config struct {
+	Next             string
+	Previous         string
+}
 
 type Command struct {
 	name        string
@@ -11,17 +21,24 @@ type Command struct {
 	callback     func() error
 }
 
-var commands = map[string]Command{
-	"help": {
-		name:        "help",
-		description: "Show the help message",
-		callback:    commandHelp,
-	},
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	},
+func getCommands() map[string]Command {
+	return map[string]Command{
+		"map": {
+			name:        "map",
+			description: "Show the names of 20 location areas. Each subsequent call will show the next 20 location areas.",
+			callback:    commandMap,
+		},
+		"help": {
+			name:        "help",
+			description: "Show the help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
 
 func main() {
@@ -32,15 +49,11 @@ func main() {
 		reader.Scan()
 		input = reader.Text()
 		words := cleanInput(input)
-		commandFound := false
-		for i := range words {
-			if cmd, ok := commands[words[i]]; ok {
-				commandFound = true
-				cmd.callback()
-			}
-		}
-		if !commandFound {
-			fmt.Printf("Unknown command\n")
+
+		if cmd, ok := getCommands()[words[0]]; ok {
+			cmd.callback()
+		} else {
+			fmt.Printf("Unknown command: %s\n", words[0])
 		}
 	}
 }
@@ -50,11 +63,30 @@ func cleanInput(text string) []string {
 	return words
 }
 
+func commandMap() error {
+	locationAreaEndpoint := fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?offset=%d&limit=%d", 0, 20)
+	res, err := http.Get(locationAreaEndpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
+	return nil
+}
+
 func commandHelp() error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:\n")
-	fmt.Println("  help: Show this help message")
-	fmt.Println("  exit: Exit the Pokedex")
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
 	return nil
 }
 
